6fd0be16afd635b59ef823c955b4dcd13f69c22f
liam2292 <liamakpata@gmail.com>
2023-06-26 19:22:52 -0700

    initialized about-me,contact-page, and my-websites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MyWebsitesComponent } from './my-websites/my-websites.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    AboutMeComponent,
+    MyWebsitesComponent
   ],
   imports: [
     BrowserModule,
