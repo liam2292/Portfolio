0d448d2fab2ec3149b403758365d19198e7e2a3f
liam2292 <liamakpata@gmail.com>
2023-06-26 18:40:05 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
